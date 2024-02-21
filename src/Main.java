fe0669752cf1cf5a61bf1c8bed51bf64faac4f23
Slava-82 <utsznslava@gmail.com>
2024-02-21 18:57:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    double ticket = 13676.99;
+    int bonus =  20;
+    int mile = (int) ticket / bonus;
+
+    System.out.println("Ваш бонус составляет " + mile + " mile");
+
+    }
+}
