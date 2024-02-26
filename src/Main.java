8caeec150461684fdf86ecf201488fd2e6e34696
Slava-82 <utsznslava@gmail.com>
2024-02-26 11:24:03 +0300

    ClassBonusMileService

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        double ticket = 13676.99;
-        int bonus = 20;
-        int mile = (int) ticket / bonus;
-
-        System.out.println("Ваш бонус составляет " + mile + " mile");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
 
     }
 }
